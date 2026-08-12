6cfebb4d6d01fd2c68eaddbfb5e4838dbca912c3
Mohamad Matar <mohamad.matar@student.arteveldehs.be>
2026-08-12 21:33:10 +0200

    accessibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 
 import { Geist } from "next/font/google";
+import Script from "next/script";
 
 const geist = Geist({
   subsets: ["latin"],
@@ -14,8 +15,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
+    <html lang="nl" suppressHydrationWarning>
       <body className={geist.variable}>
+        <Script
+          id="set-document-language"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                var path = window.location.pathname;
+                var lang = path.startsWith('/en') ? 'en' : 'nl';
+                document.documentElement.lang = lang;
+              })();
+            `,
+          }}
+        />
+
         {children}
       </body>
     </html>
